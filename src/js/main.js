95a0743ebddf904d9eeffa7353fd3f3d09317ba5
PanArtemG <artem253510@gmail.com>
2020-02-22 02:21:41 +0200

    'upload'

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log('HELLO');
+console.log('HELLO')
+const btnUploadFile = document.querySelector('.upload_input-btn')
+const inpUpload = document.getElementById('uploadInput')
+const uploadFiles = []
+
+btnUploadFile.addEventListener('click', event => {
+  // console.log(event.target)
+  event.preventDefault()
+  inpUpload.click()
+})
+
+
+
+inpUpload.addEventListener('change', ev => {
+  const file = ev.target.files[0]
+  uploadFiles.push(file)
+  console.log(uploadFiles);
+})
+
+function adNewFile() {
+
+}
